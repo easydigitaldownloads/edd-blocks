c33fb464f7a6c22d43c8e6a8f108e0d94901ca17
Andrew Munro <andrew@amdrew.com>
2018-08-08 20:18:25 +1200

    Add initial index.php for frontend display

---
+++ includes/blocks/download-categories/index.php
@@ -0,0 +1,26 @@
+<?php
+
+function edd_blocks_render_block_download_categories( $attributes = array() ) {
+	ob_start();
+	
+	$display = ob_get_clean();
+
+	return $display;
+}
+
+/**
+ * Registers the `easydigitaldownloads/downloads` block on server.
+ */
+function edd_blocks_register_block_download_categories() {
+
+	if ( ! function_exists( 'register_block_type' ) ) {
+		return;
+	}
+
+	register_block_type( 'easydigitaldownloads/download-categories', array(
+		'attributes' => array(
+		),
+		'render_callback' => 'edd_blocks_render_block_download_categories',
+	) );
+}
+add_action( 'init', 'edd_blocks_register_block_download_categories' );
