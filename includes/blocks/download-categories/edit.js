6a2523f9cddc76a69f314eef5eee33d8ad478983
Andrew Munro <andrew@amdrew.com>
2018-08-09 00:06:42 +1200

    Only show description when toggled

--- includes/blocks/download-categories/edit.js
+++ includes/blocks/download-categories/edit.js
@@ -87,10 +87,15 @@ class DownloadCategoriesEdit extends Component {
 
 	renderDownloadCategoryListItem( category ) {
 
+		const { showDescription } = this.props.attributes;
+
 		return (
 			<div key={ category.id }>
 				<a href={ category.link } target="_blank">{ this.renderDownloadCategoryName( category ) }</a>
+
+				{ showDescription &&
 				<p>{ category.description }</p>
+				}
 			</div>
 		);
 	}
