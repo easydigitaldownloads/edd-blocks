18436ad17fa9d146086528bc07ab9cb389794dba
Andrew Munro <andrew@amdrew.com>
2018-08-08 21:05:01 +1200

    Fetch download categories and store in state

--- includes/blocks/download-categories/edit.js
+++ includes/blocks/download-categories/edit.js
@@ -1,15 +1,61 @@
+import { stringify } from 'querystringify';
+
 const {	Component, Fragment } = wp.element;
+
+const apiFetch = wp.apiFetch;
 
 class DownloadCategoriesEdit extends Component {
 	constructor() {
 		super( ...arguments );
+
+		this.state = {
+			downloadCategories: [],
+		}
+	}
+
+	componentDidMount() {
+		this.fetchDownloadCategories();
+	}
+
+	componentWillUnmount() {
+		delete this.downloadCategoriesRequest;
+	}
+
+	fetchDownloadCategories() {
+
+		const request = apiFetch( {
+			path: `/wp/v2/download_category?${ stringify( {
+				per_page: -1,
+			} ) }`,
+		} );
+
+		
+		request.then( ( downloadCategories ) => {
+
+			if ( this.downloadCategoriesRequest !== request ) {
+				return;
+			}
+
+			this.setState( { downloadCategories } );
+
+		} );
+
+		this.downloadCategoriesRequest = request;
+
+	}
+
+	renderDownloadCategories() {
+		return 'Download categories to show here';
 	}
 
 	render() {
+
+		console.log( this.state.downloadCategories );
+
 		return (
 			<Fragment>
 				<div className={ this.props.className }>
-					Download categories to show here
+					{ this.renderDownloadCategories() }
 				</div>
 			</Fragment>
 		);
