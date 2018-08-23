7f5936e9e467e94be8176a25faa98d29cb6ee221
Andrew Munro <andrew@amdrew.com>
2018-08-23 23:16:02 +1200

    Build out rest of function based on [downloads] shortcode

--- includes/functions.php
+++ includes/functions.php
@@ -8,12 +8,14 @@
  */
 function edd_blocks_downloads_list( $atts = array(), $type = '' ) {
 
+	// Set up the base $query.
 	$query = array(
 		'post_type'      => 'download',
 		'orderby'        => $atts['orderby'],
-		'order'          => $atts['order']
+		'order'          => $atts['order'],
 	);
 
+	// Pagination.
 	if ( filter_var( $atts['pagination'], FILTER_VALIDATE_BOOLEAN ) || ( ! filter_var( $atts['pagination'], FILTER_VALIDATE_BOOLEAN ) && $atts[ 'number' ] ) ) {
 
 		$query['posts_per_page'] = (int) $atts['number'];
@@ -30,6 +32,7 @@ function edd_blocks_downloads_list( $atts = array(), $type = '' ) {
 		$atts['pagination'] = false;
 	}
 
+	// Order by.
 	switch ( $atts['orderby'] ) {
 		case 'price':
 			$query['meta_key'] = 'edd_price';
@@ -71,10 +74,179 @@ function edd_blocks_downloads_list( $atts = array(), $type = '' ) {
 		break;
 	}
 
-	// if ( ! empty( $atts['ids'] ) ) {
-	// 	$query['post__in'] = explode( ',', $atts['ids'] );
-	// }
+	if ( isset( $atts['tags'] ) || isset( $atts['category'] ) || isset( $atts['exclude_category'] ) || isset( $atts['exclude_tags'] ) ) {
 
+		$query['tax_query'] = array(
+			'relation' => $atts['relation']
+		);
+
+		if ( $atts['tags'] ) {
+
+			$tag_list = explode( ',', $atts['tags'] );
+
+			foreach ( $tag_list as $tag ) {
+
+				$t_id  = (int) $tag;
+				$is_id = is_int( $t_id ) && ! empty( $t_id );
+
+				if ( $is_id ) {
+
+					$term_id = $tag;
+
+				} else {
+
+					$term = get_term_by( 'slug', $tag, 'download_tag' );
+
+					if ( ! $term ) {
+						continue;
+					}
+
+					$term_id = $term->term_id;
+				}
+
+				$query['tax_query'][] = array(
+					'taxonomy' => 'download_tag',
+					'field'    => 'term_id',
+					'terms'    => $term_id
+				);
+			}
+
+		}
+
+		if ( $atts['category'] ) {
+
+			$categories = explode( ',', $atts['category'] );
+
+			foreach ( $categories as $category ) {
+
+				$t_id  = (int) $category;
+				$is_id = is_int( $t_id ) && ! empty( $t_id );
+
+				if ( $is_id ) {
+					$term_id = $category;
+				} else {
+					$term = get_term_by( 'slug', $category, 'download_category' );
+
+					if ( ! $term ) {
+						continue;
+					}
+
+					$term_id = $term->term_id;
+				}
+
+				$query['tax_query'][] = array(
+					'taxonomy' => 'download_category',
+					'field'    => 'term_id',
+					'terms'    => $term_id,
+				);
+
+			}
+
+		}
+
+		if ( $atts['exclude_category'] ) {
+
+			$categories = explode( ',', $atts['exclude_category'] );
+
+			foreach ( $categories as $category ) {
+
+				$t_id  = (int) $category;
+				$is_id = is_int( $t_id ) && ! empty( $t_id );
+
+				if ( $is_id ) {
+
+					$term_id = $category;
+
+				} else {
+
+					$term = get_term_by( 'slug', $category, 'download_category' );
+
+					if ( ! $term ) {
+						continue;
+					}
+
+					$term_id = $term->term_id;
+				}
+
+				$query['tax_query'][] = array(
+					'taxonomy' => 'download_category',
+					'field'    => 'term_id',
+					'terms'    => $term_id,
+					'operator' => 'NOT IN'
+				);
+			}
+
+		}
+
+		if ( $atts['exclude_tags'] ) {
+
+			$tag_list = explode( ',', $atts['exclude_tags'] );
+
+			foreach ( $tag_list as $tag ) {
+
+				$t_id  = (int) $tag;
+				$is_id = is_int( $t_id ) && ! empty( $t_id );
+
+				if( $is_id ) {
+
+					$term_id = $tag;
+
+				} else {
+
+					$term = get_term_by( 'slug', $tag, 'download_tag' );
+
+					if ( ! $term ) {
+						continue;
+					}
+
+					$term_id = $term->term_id;
+				}
+
+				$query['tax_query'][] = array(
+					'taxonomy' => 'download_tag',
+					'field'    => 'term_id',
+					'terms'    => $term_id,
+					'operator' => 'NOT IN'
+				);
+
+			}
+
+		}
+	}
+
+	if ( isset( $atts['exclude_tags'] ) || isset( $atts['exclude_category'] ) ) {
+		$query['tax_query']['relation'] = 'AND';
+	}
+
+	if ( isset( $atts['author'] ) ) {
+		$authors = explode( ',', $atts['author'] );
+		if ( ! empty( $authors ) ) {
+			$author_ids = array();
+			$author_names = array();
+
+			foreach ( $authors as $author ) {
+				if ( is_numeric( $author ) ) {
+					$author_ids[] = $author;
+				} else {
+					$user = get_user_by( 'login', $author );
+					if ( $user ) {
+						$author_ids[] = $user->ID;
+					}
+				}
+			}
+
+			if ( ! empty( $author_ids ) ) {
+				$author_ids      = array_unique( array_map( 'absint', $author_ids ) );
+				$query['author'] = implode( ',', $author_ids );
+			}
+		}
+	}
+
+	if ( ! empty( $atts['ids'] ) ) {
+		$query['post__in'] = explode( ',', $atts['ids'] );
+	}
+	
+	// Paged.
 	if ( get_query_var( 'paged' ) ) {
 		$query['paged'] = get_query_var('paged');
 	} else if ( get_query_var( 'page' ) ) {
