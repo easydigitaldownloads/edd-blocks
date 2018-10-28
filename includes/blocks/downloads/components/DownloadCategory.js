24befec5edf3111ae78c22acb802e410f2152911
Andrew Munro <andrew@amdrew.com>
2018-10-28 14:17:35 +1300

    Add <DownloadCategory /> component

---
+++ includes/blocks/downloads/components/DownloadCategory.js
@@ -0,0 +1,39 @@
+import Image from './Image';
+import Title from './Title';
+import Description from './Description';
+
+const DownloadCategory = (props) => {
+
+	const { showThumbnails, showTitle, showCount, showDescription } = props.attributes;
+
+	const { name, link, count, description } = props.category;
+	const image = props.category.meta.image;
+
+	return (
+		<div className="edd-download-category">
+			<Image 
+				image={image} 
+				showThumbnails={showThumbnails} 
+			/>
+			{ showTitle && 
+			<Title 
+				title={name} 
+				link={link} 
+				className="edd-download-category-title"
+				showCount={showCount}
+				count={count}
+				type="download-category"
+			/>
+			}
+			{ showDescription &&
+			<Description 
+				description={description} 
+				showDescription={showDescription}
+			/>
+			}
+		</div>
+	)
+
+}
+
+export default DownloadCategory;
