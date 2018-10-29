32052111c52dadefa2679b98deeeb2ec35869f3d
Andrew Munro <andrew@amdrew.com>
2018-10-29 16:40:53 +1300

    Add tag’s description
    + hook it up to show description toggle controller

--- includes/blocks/downloads/components/DownloadTag.js
+++ includes/blocks/downloads/components/DownloadTag.js
@@ -1,8 +1,9 @@
 import Title from './Title';
+import Description from './Description';
 
 const DownloadTag = (props) => {
-
-	const { name, link } = props.tag;
+	const { showDescription } = props.attributes;
+	const { name, link, description } = props.tag;
 
 	return (
 		<div className="edd-download-tag">
@@ -12,6 +13,13 @@ const DownloadTag = (props) => {
 				className="edd_download_title"
 				type="download-tag"
 			/>
+			{ showDescription &&
+			<Description 
+				description={description}
+				showDescription={showDescription}
+				className="edd-download-tag-description"
+			/>
+			}
 		</div>
 	)
 
