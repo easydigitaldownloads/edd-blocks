e8d2ba2210943b7319aa590745dc0bb13bd741f1
Andrew Munro <andrew@amdrew.com>
2018-10-29 16:36:31 +1300

    Render tag title with link, className and type

--- includes/blocks/downloads/components/DownloadTag.js
+++ includes/blocks/downloads/components/DownloadTag.js
@@ -1,8 +1,17 @@
+import Title from './Title';
+
 const DownloadTag = (props) => {
+
+	const { name, link } = props.tag;
 
 	return (
 		<div className="edd-download-tag">
-			Download tag placeholder
+			<Title 
+				title={name} 
+				link={link} 
+				className="edd_download_title"
+				type="download-tag"
+			/>
 		</div>
 	)
 
