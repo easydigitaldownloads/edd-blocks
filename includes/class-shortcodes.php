953ca2e9163149529ffd46437f1551fa3daa0777
Andrew Munro <andrew@amdrew.com>
2018-11-05 10:36:09 +1300

    Add initial [download_categories] shortcode with some attributes

--- includes/class-shortcodes.php
+++ includes/class-shortcodes.php
@@ -2,7 +2,27 @@
 
 class EDD_Blocks_Shortcodes {
 
-	public function __construct() {}
+	public function __construct() {
+		add_shortcode( 'download_categories', array( $this, 'download_categories' ) );
+	}
 
+	public function download_categories( $atts, $content = null ) {
+
+		$atts = shortcode_atts(
+			array(
+				'type'            => 'download_categories',
+				'number'          => 9,
+				'showTitle'       => true,
+				'showThumbnails'  => true,
+				'showDescription' => true,
+				'showEmpty'       => false
+			),
+			$atts,
+			'download_categories'
+		);
+
+		return edd_blocks_download_taxonomy_list( $atts );
+	}
+	
 }
 new EDD_Blocks_Shortcodes;
