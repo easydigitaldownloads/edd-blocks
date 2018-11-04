78856cbe29c20e441105243e73adb5fd6fcb0b09
Andrew Munro <andrew@amdrew.com>
2018-11-05 10:38:51 +1300

    Add initial [download_tags] shortcode

--- includes/class-shortcodes.php
+++ includes/class-shortcodes.php
@@ -4,6 +4,7 @@ class EDD_Blocks_Shortcodes {
 
 	public function __construct() {
 		add_shortcode( 'download_categories', array( $this, 'download_categories' ) );
+		add_shortcode( 'download_tags', array( $this, 'download_tags' ) );
 	}
 
 	public function download_categories( $atts, $content = null ) {
@@ -23,6 +24,24 @@ class EDD_Blocks_Shortcodes {
 
 		return edd_blocks_download_taxonomy_list( $atts );
 	}
-	
+
+	public function download_tags( $atts, $content = null ) {
+
+		$atts = shortcode_atts(
+			array(
+				'type'            => 'download_tags',
+				'number'          => 9,
+				'showTitle'       => true,
+				'showThumbnails'  => true,
+				'showDescription' => true,
+				'showEmpty'       => false
+			),
+			$atts,
+			'download_tags'
+		);
+
+		return edd_blocks_download_taxonomy_list( $atts );
+	}
+
 }
 new EDD_Blocks_Shortcodes;
