4f30ea171a7f598c310210fde529348c5292cddf
Andrew Munro <andrew@amdrew.com>
2018-11-05 13:44:58 +1300

    Refactor [edd_download_terms] shortcode

--- includes/class-shortcodes.php
+++ includes/class-shortcodes.php
@@ -40,8 +40,35 @@ class EDD_Blocks_Shortcodes {
 	}
 
 	public function download_tags( $atts, $content = null ) {
+		$atts = shortcode_atts(
+			array(
+				'thumbnails'  => true,
+				'title'       => true,
+				'description' => true,
+				'show_empty'  => false,
+				'columns'     => 3,
+				'count'       => true,
+				'orderby'     => 'count',
+				'order'       => 'DESC',
+			),
+			$atts,
+			'download_tags'
+		);
 
+		$args = array(
+			'taxonomy'    => 'download_tag',
+			'thumbnails'  => 'false' === $atts['thumbnails'] ? false : $atts['thumbnails'],
+			'title'       => 'false' === $atts['title'] ? false : $atts['title'],
+			'description' => 'false' === $atts['description'] ? false : $atts['description'],
+			'show_empty'  => 'true' === $atts['thumbnails'] ? true : $atts['thumbnails'],
+			'columns'     => isset( $atts['columns'] ) ? $atts['columns'] : $atts['columns'],
+			'count'       => 'false' === $atts['count'] ? false : $atts['count'],
+			'orderby'     => isset( $atts['orderby'] ) ? $atts['orderby'] : $atts['orderby'],
+			'order'       => isset( $atts['order'] ) ? $atts['order'] : $atts['order'],
+		);
+
+		return edd_download_terms( $args );
 	}
-	
+
 }
 new EDD_Blocks_Shortcodes;
