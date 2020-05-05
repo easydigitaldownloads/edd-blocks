920fec05111d916b6f6db304cb8963d253498829
LisaCee <lisacanini@sandhillsdev.com>
2020-05-05 12:36:49 -0700

    added external dependencies to webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,62 +1,73 @@
-const path = require('path');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+// WordPress dependencies
 
-const stylesCSSPlugin = new ExtractTextPlugin( {
-	filename: './styles.css',
-} );
+const defaultConfig = require( '@wordpress/scripts/config/webpack.config.js' );
 
-const adminCSSPlugin = new ExtractTextPlugin( {
-	filename: './admin.css',
-} );
+// External dependencies
+const webpack = require( 'webpack' );
+const MiniCssExtractPlugin = require( 'mini-css-extract-plugin' );
+const FixStyleOnlyEntriesPlugin = require( 'webpack-fix-style-only-entries' );
+const OptimizeCssAssetsPlugin = require( 'optimize-css-assets-webpack-plugin' );
 
-// Configuration for the ExtractTextPlugin.
-const extractConfig = {
-	use: [
-		{ loader: 'raw-loader' },
-		{
-			loader: 'postcss-loader',
-			options: {
-				plugins: [
-					require( 'autoprefixer' ),
-				],
-			},
-		},
-		{
-			loader: 'sass-loader',
-			query: {
-				outputStyle: 'production' === process.env.NODE_ENV ? 'compressed' : 'nested',
-			},
-		},
-	],
-};
 
-module.exports = {
-	entry: { main: './src/index.js' },
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: 'main.js'
-	},
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				use: {
-					loader: 'babel-loader'
-				}
-			},
-			{
-				test: /styles\.s?css$/,
-				use: stylesCSSPlugin.extract( extractConfig ),
-			},
-			{
-				test: /admin\.s?css$/,
-				use: adminCSSPlugin.extract( extractConfig ),
-			},
-		]
-	},
-	plugins: [
-		stylesCSSPlugin,
-		adminCSSPlugin
-	]
-};
+// const path = require('path');
+// const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+// const stylesCSSPlugin = new ExtractTextPlugin( {
+// 	filename: './styles.css',
+// } );
+
+// const adminCSSPlugin = new ExtractTextPlugin( {
+// 	filename: './admin.css',
+// } );
+
+// // Configuration for the ExtractTextPlugin.
+// const extractConfig = {
+// 	use: [
+// 		{ loader: 'raw-loader' },
+// 		{
+// 			loader: 'postcss-loader',
+// 			options: {
+// 				plugins: [
+// 					require( 'autoprefixer' ),
+// 				],
+// 			},
+// 		},
+// 		{
+// 			loader: 'sass-loader',
+// 			query: {
+// 				outputStyle: 'production' === process.env.NODE_ENV ? 'compressed' : 'nested',
+// 			},
+// 		},
+// 	],
+// };
+
+// module.exports = {
+// 	entry: { main: './src/index.js' },
+// 	output: {
+// 		path: path.resolve(__dirname, 'dist'),
+// 		filename: 'main.js'
+// 	},
+// 	module: {
+// 		rules: [
+// 			{
+// 				test: /\.js$/,
+// 				exclude: /node_modules/,
+// 				use: {
+// 					loader: 'babel-loader'
+// 				}
+// 			},
+// 			{
+// 				test: /styles\.s?css$/,
+// 				use: stylesCSSPlugin.extract( extractConfig ),
+// 			},
+// 			{
+// 				test: /admin\.s?css$/,
+// 				use: adminCSSPlugin.extract( extractConfig ),
+// 			},
+// 		]
+// 	},
+// 	plugins: [
+// 		stylesCSSPlugin,
+// 		adminCSSPlugin
+// 	]
+// };
