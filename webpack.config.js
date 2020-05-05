32aeaed3e6d598f9df577a72bd4c2725154f0933
LisaCee <lisacanini@sandhillsdev.com>
2020-05-05 13:08:30 -0700

    updated webpack.config with @wordpress/scripts

--- webpack.config.js
+++ webpack.config.js
@@ -3,71 +3,65 @@
 const defaultConfig = require( '@wordpress/scripts/config/webpack.config.js' );
 
 // External dependencies
+const path = require('path');
 const webpack = require( 'webpack' );
 const MiniCssExtractPlugin = require( 'mini-css-extract-plugin' );
 const FixStyleOnlyEntriesPlugin = require( 'webpack-fix-style-only-entries' );
 const OptimizeCssAssetsPlugin = require( 'optimize-css-assets-webpack-plugin' );
 
+module.exports = {
+	...defaultConfig,
+	devtool: 'source-map',
+	externals: {
+		jquery: 'jQuery',
+		$: 'jQuery'
+	},
+	entry: {
+		// JS
+		main: './dist/main.js',
+		// CSS
+		style: './dist/styles.css',
+		// 'admin-style': './dist/admin.css',
+	},
+	output: {
+		filename: 'dist/[name].js',
+		path: __dirname
+	},
+	plugins: [
+		new FixStyleOnlyEntriesPlugin(),
+		new OptimizeCssAssetsPlugin(),
+		new webpack.ProvidePlugin( {
+			Promise: 'es6-promise-promise',
+			$: 'jquery',
+		} ),
+		new MiniCssExtractPlugin( {
+			filename: 'dist/styles.css',
+		} )
+	],
+	module: {
+		rules: [
+			...defaultConfig.module.rules,
+			{
+				test: /\.css$/,
+				use: [
+					MiniCssExtractPlugin.loader,
+					{
+						loader: 'css-loader',
+						options: {
+							importLoaders: 1,
+						},
+					},
+					{
+						loader: 'postcss-loader',
+						options: {
+							plugins: () => [
+								require('autoprefixer'),
+							],
+						},
+					},
+				],
+			},
+		],
+	},
+}
 
-// const path = require('path');
-// const ExtractTextPlugin = require('extract-text-webpack-plugin');
-
-// const stylesCSSPlugin = new ExtractTextPlugin( {
-// 	filename: './styles.css',
-// } );
-
-// const adminCSSPlugin = new ExtractTextPlugin( {
-// 	filename: './admin.css',
-// } );
-
-// // Configuration for the ExtractTextPlugin.
-// const extractConfig = {
-// 	use: [
-// 		{ loader: 'raw-loader' },
-// 		{
-// 			loader: 'postcss-loader',
-// 			options: {
-// 				plugins: [
-// 					require( 'autoprefixer' ),
-// 				],
-// 			},
-// 		},
-// 		{
-// 			loader: 'sass-loader',
-// 			query: {
-// 				outputStyle: 'production' === process.env.NODE_ENV ? 'compressed' : 'nested',
-// 			},
-// 		},
-// 	],
-// };
-
-// module.exports = {
-// 	entry: { main: './src/index.js' },
-// 	output: {
-// 		path: path.resolve(__dirname, 'dist'),
-// 		filename: 'main.js'
-// 	},
-// 	module: {
-// 		rules: [
-// 			{
-// 				test: /\.js$/,
-// 				exclude: /node_modules/,
-// 				use: {
-// 					loader: 'babel-loader'
-// 				}
-// 			},
-// 			{
-// 				test: /styles\.s?css$/,
-// 				use: stylesCSSPlugin.extract( extractConfig ),
-// 			},
-// 			{
-// 				test: /admin\.s?css$/,
-// 				use: adminCSSPlugin.extract( extractConfig ),
-// 			},
-// 		]
-// 	},
-// 	plugins: [
-// 		stylesCSSPlugin,
-// 		adminCSSPlugin
-// 	]
-// };
